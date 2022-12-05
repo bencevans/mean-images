fbbf1442bffc3f479ad57b261d67e04d4347dee5
Ben Evans <ben@bluechimp.io>
2022-12-05 13:02:28 +0000

    std dev

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,38 @@ fn main() {
         let avg_green = total_green / total_pixels;
         let avg_blue = total_blue / total_pixels;
 
-        Some((avg_red, avg_green, avg_blue))
+        // Calculate Standard Deviation
+        let dev_red = (img
+            .pixels()
+            .map(|(_, _, rgba)| {
+                let diff = rgba[0] as f32 - avg_red;
+                diff * diff
+            })
+            .sum::<f32>()
+            / total_pixels)
+            .sqrt();
+
+        let dev_green = (img
+            .pixels()
+            .map(|(_, _, rgba)| {
+                let diff = rgba[1] as f32 - avg_green;
+                diff * diff
+            })
+            .sum::<f32>()
+            / total_pixels)
+            .sqrt();
+
+        let dev_blue = (img
+            .pixels()
+            .map(|(_, _, rgba)| {
+                let diff = rgba[2] as f32 - avg_blue;
+                diff * diff
+            })
+            .sum::<f32>()
+            / total_pixels)
+            .sqrt();
+
+        Some((avg_red, avg_green, avg_blue, dev_red, dev_green, dev_blue))
     });
 
     let results = results.collect::<Vec<_>>();
@@ -48,11 +79,31 @@ fn main() {
     let mut avg_green: f32 = 0.0;
     let mut avg_blue: f32 = 0.0;
 
-    results.into_iter().for_each(|(r, g, b)| {
+    let mut dev_red: f32 = 0.0;
+    let mut dev_green: f32 = 0.0;
+    let mut dev_blue: f32 = 0.0;
+
+    results.into_iter().for_each(|(r, g, b, d_r, d_g, d_b)| {
         avg_red += r / total_images;
         avg_green += g / total_images;
         avg_blue += b / total_images;
+
+        dev_red += d_r / total_images;
+        dev_green += d_g / total_images;
+        dev_blue += d_b / total_images;
     });
 
-    println!("R: {} G: {} B: {}", avg_red, avg_green, avg_blue,);
+    println!(
+        "mean: {}, {}, {} std: {}, {}, {}",
+        avg_red, avg_green, avg_blue, dev_red, dev_green, dev_blue
+    );
+    println!(
+        "mean: {}, {}, {} std: {}, {}, {}",
+        avg_red / 255.0,
+        avg_green / 255.0,
+        avg_blue / 255.0,
+        dev_red / 255.0,
+        dev_green / 255.0,
+        dev_blue / 255.0
+    );
 }
